eb6bbef54e54c9531c8ed24e943b4cdfcc98aad9
Liu Dongmiao <liudongmiao@gmail.com>
2019-10-27 23:27:17 +0800

    update for 8.2.3.2 and 8.2.3.4

--- src/main/java/me/piebridge/VolatileXReorder8234.java
+++ src/main/java/me/piebridge/VolatileXReorder8234.java
@@ -8,24 +8,24 @@ import org.openjdk.jcstress.annotations.State;
 import org.openjdk.jcstress.infra.results.II_Result;
 
 @JCStressTest
-@Outcome(id = {".*"}, expect = Expect.ACCEPTABLE, desc = "Normal outcome")
-@Outcome(id = {"1, 1"}, expect = Expect.ACCEPTABLE_INTERESTING, desc = "Abnormal outcome")
+@Outcome(id = {"0, 1", "1, 0", "1, 1"}, expect = Expect.ACCEPTABLE, desc = "Normal outcome")
+@Outcome(id = {"0, 0"}, expect = Expect.ACCEPTABLE_INTERESTING, desc = "Abnormal outcome")
 @State
-public class Reorder8234Variant {
+public class VolatileXReorder8234 {
 
-    private int x = 0;
+    private volatile int x = 0;
     private int y = 0;
 
     @Actor
     public void process0(II_Result r) {
-        r.r2 = y;
         x = 1;
+        r.r2 = y;
     }
 
     @Actor
     public void process1(II_Result r) {
-        r.r1 = x;
         y = 1;
+        r.r1 = x;
     }
 
 }
--- src/main/java/me/piebridge/VolatileYReorder8234.java
+++ src/main/java/me/piebridge/VolatileYReorder8234.java
@@ -8,25 +8,24 @@ import org.openjdk.jcstress.annotations.State;
 import org.openjdk.jcstress.infra.results.II_Result;
 
 @JCStressTest
-@Outcome(id = {"0, 0", "0, 1", "0, 2", "1, 2"}, expect = Expect.ACCEPTABLE, desc = "Normal outcome")
-@Outcome(id = {"1, 1"}, expect = Expect.ACCEPTABLE_INTERESTING, desc = "Abnormal outcome")
+@Outcome(id = {"0, 1", "1, 0", "1, 1"}, expect = Expect.ACCEPTABLE, desc = "Normal outcome")
+@Outcome(id = {"0, 0"}, expect = Expect.ACCEPTABLE_INTERESTING, desc = "Abnormal outcome")
 @State
-public class VolatileXReorder8232ReleaseNA {
+public class VolatileYReorder8234 {
 
-    private volatile int x = 0;
-    private int y = 0;
+    private int x = 0;
+    private volatile int y = 0;
 
     @Actor
     public void process0(II_Result r) {
-        // can reorder? YES
         x = 1;
-        y = 1;
+        r.r2 = y;
     }
 
     @Actor
     public void process1(II_Result r) {
-        r.r1 = y;
-        r.r2 = x + r.r1;
+        y = 1;
+        r.r1 = x;
     }
 
 }
